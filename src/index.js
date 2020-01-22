5a647d35c1f2a8e0fd5363d34bc8daf8ab85fa2e
Victor Eyer <eyervictor@gmail.com>
2020-01-22 00:01:03 -0300

    Heroku deploy

--- src/index.js
+++ src/index.js
@@ -15,4 +15,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333,  () => console.log("listening..."));
+app.listen(process.env.PORT || 3333,  () => console.log("listening..."));
