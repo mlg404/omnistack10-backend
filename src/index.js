3dea5bdd091379968b9d283a2096b4fd3e8a7e4e
Victor Eyer <eyervictor@gmail.com>
2020-01-22 15:03:20 -0300

    DotEnv configs

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistackeyer:omnistackeyer@cluster0-2lp8f.mongodb.net/omnistack?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -15,4 +16,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3333,  () => console.log("listening..."));
+app.listen(process.env.PORT,  () => console.log("listening..."));
